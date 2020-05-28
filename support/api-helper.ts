77002badae44f9229bd9f3e5827311c0dd36058d
Kapil Sethi <kapil.sethi9@gmail.com>
2020-05-28 23:50:46 +1000

    using axois for http requests - wip

--- support/api-helper.ts
+++ support/api-helper.ts
@@ -1,88 +1,97 @@
-import * as request from "request-promise";
+import axios from 'axios';
 import { getLogger } from "log4js";
 const logger = getLogger();
 logger.level = "debug";
 
 class ApiHelper {
-    async getRequestMethod(url, apiKey) {
-        const options = {
-            method: 'GET',
-            url: url,
-            json: true,
-            headers: {
-                'content-type': 'application/json'
-            },
-            qs: {
-                APPID: apiKey
-            }
-        };
-        try {
-            const response = await request.get(options);
-            logger.info('get request response -->', response);
-            return response;
-        }
-        catch (error) {
-            logger.info('get request error -->', error);
-            return error;
-        }
-    }
+    timeout = 10000;
 
-    async postRequestMethod(url, apiKey, requestBody) {
-        let responseStatusCode;
-        const options = {
-            method: 'POST',
-            url: url,
-            json: true,
-            headers: {
-                'content-type': 'application/json'
-            },
-            qs: {
-                APPID: apiKey
-            },
-            body:{
+    async postRequestMethod(url, requestBody, useApiKey) {
+        try {
+            const response = await axios({
+                method: 'post',
+                url: url,
+                headers: {
+                    'content-type': 'application/json'
+                },
+                params: {
+                    APPID: await this.getApiLKey(useApiKey)
+                },
+                timeout: this.timeout,
+                data: {
                     "external_id": requestBody.external_id,
                     "name": requestBody.name,
                     "latitude": requestBody.latitude,
                     "longitude": requestBody.longitude,
                     "altitude": requestBody.altitude
-            }
-        };
-        try {
-            const res = await request.post(options, function(error, response) {
-                logger.info('post request response -->', response);
-                responseStatusCode = response.statusCode;
+                }
             });
-            logger.info('post request statusCode -->', responseStatusCode);
-            return {
-                response: res,
-                statusCode: responseStatusCode
-            };
+            return response;
         }
-        catch (error) {
-            return error;
+        catch(err) {
+            return this.catchError(err);
         }
     }
 
-    async deleteRequestMethod(url, apiKey) {
-        const options = {
-            method: 'DELETE',
-            url: url,
-            json: true,
-            headers: {
-                'content-type': 'application/json'
-            },
-            qs: {
-                APPID: apiKey
-            }
-        };
+    async getRequestMethod(url) {
         try {
-            const response = await request.delete(options);
-            logger.info('delete request response -->', response);
+            const response = await axios({
+                method: 'get',
+                url: url,
+                // baseURL: testData["url"],
+                headers: {
+                    'content-type': 'application/json'
+                },
+                params: {
+                    APPID: await this.getApiLKey(true)
+                },
+                timeout: this.timeout
+            });
+            return response.data;
+        }
+        catch (err) {
+            return this.catchError(err);
+        }
+    }
+
+    async deleteRequestMethod(url) {
+        try {
+            const response = await axios({
+                method: 'delete',
+                url: url,
+                headers: {
+                    'content-type': 'application/json'
+                },
+                params: {
+                    APPID: await this.getApiLKey(true)
+                },
+                timeout: this.timeout
+            });
             return response;
         }
-        catch (error) {
-            logger.info('delete request error -->', error);
-            return error;
+        catch(err) {
+            return this.catchError(err);
+        }
+    }
+
+    private async getApiLKey(useApiKey) {
+        return useApiKey? process.env.API_KEY : '';
+    }
+
+    private catchError(err) {
+        if (err.response) {
+            // the request went through and a response was returned
+            // status code in 3xx / 4xx / 5xx range
+            logger.info(`request went through and an error response was returned with\n
+                message: ${err.response.data.message} and\nstatus code: ${err.response.status}`);
+            return err.response;
+
+        } else if (err.request) {
+            logger.info(`request was made but server returned no response\n ${err.request}`);
+            throw err.request;
+        } else {
+            logger.info(`somthing went wrong in setting up the request\n ${err.message}`);
+            throw err.message;
         }
     }
 }
